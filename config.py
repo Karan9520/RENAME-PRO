417065c11d585f7ddc57982acc1f26ba8d65a11c
Karan singh <68398500+Karan9520@users.noreply.github.com>
2024-07-09 00:49:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
 import os, re
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6755590776:AAEQV7LaRwIXzk8ADbc0KXYK0BpC4YtgH8w")
+API_ID = int(os.environ.get("API_ID", "26274714"))
+API_HASH = os.environ.get("API_HASH", "426333ef463416eaa332da1d813c9105")
+OWNER = int(os.environ.get("OWNER", "7165581725"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "MoonxMoviesbot")
 
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "HxBots")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002031041556"))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "Movies4UBackupChannel")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1004259584603"))
 
-DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "renamebot-premium")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://FileRenameBot:FileRenameBot@filerenamebot.91cxsxb.mongodb.net/?retryWrites=true&w=majority&appName=FileRenameBot")
+DB_NAME = os.environ.get("DB_NAME", "FileRenameBot")
 
-STRING = os.environ.get("STRING", "")
-BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/cca849a2f63053fa3f622.jpg")
+STRING = os.environ.get("STRING", "BQB93QoAiJ9Z0ZVVdxZlrPIpgByA3vIf37mo8DDtKkURPH6AsiqbzJaE2LChJp2duvYbQTb-fQVrCuZn02Iudzry5tQsQ5exlhgw_am6ohkeYfpdx4Zmks77k5MwIjoBUrRx7M7MipaXeYsKkT8Q-g8Hxj22sSA_OjPOqtzsw35l6f2mokWNOxX209VxovafmOThbqC_8vVDVJMBoj_Dj2_AG19Lx8cuAQHpo5yq_gdmMefO-52rhQ-aiPEjqC-X-kZ4huuPw1MKmVpbsOqmokVLmeCxdD1TYLAn9rNaZyZRSLxP3X5pUSi8TYvQzf6izqlHU2Ue9Akiez-7mNLV0aUTMRz8-QAAAAGB-NTQAA")
+BOT_PIC = os.environ.get("BOT_PIC", "https://telegra.ph/file/e82bf5fe8245d8b61ec21.jpg")
 
 
 
